5edde49df481180eb9384db1d7aab58f67c5213a
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2025-10-09 00:13:11 -0600

    hay va

--- app.py
+++ app.py
@@ -4,9 +4,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    arr = ["Luis","Kemia","Gerardo","Arturo"]
-    autor ="jesus Antonio Sagarnaga Macias"
-    return render_template("index.html" , nombre=autor , amigos=arr)
+    return render_template("base.html")
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/animales_exoticos")
+def animales():
+    animal = ["Foca monje del Caribe", "Gato", "Elefante", "León", "Tigre", "Jirafa", "Canguro", "Mono", "Rinoceronte", "Hipopótamo"]
+    return render_template("animales.html", animales=animal)
+
+@app.route("/vehiculos_antiguos")
+def vehiculos():
+    return render_template("vehiculos.html")
+
+@app.route("/maravillas_mundo")
+def maravillas():
+    return render_template("maravillas.html")
+
+@app.route("/acerca_de")
+def acerca():
+    return render_template("acerca.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
